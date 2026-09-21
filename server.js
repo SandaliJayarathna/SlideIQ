b4915876a051ab664332517f7755c5110587869a
Sandali <sandali.dilru1@gmail.com>
2026-09-21 22:19:31 +0530

    Fix static files for Vercel

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const PORT = 3000;
 
 app.use(express.json({ limit: "50mb" }));
 
-app.use(express.static("."));
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "..")));
 
 app.post("/generate-pptx", async (req, res) => {
 
